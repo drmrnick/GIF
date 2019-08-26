45d009d58d7c67c6c61e21facd47f32c0540ffda
Nick Tuttle <drmrnick87@gmail.com>
2019-08-26 14:06:02 -0400

    still & response
    tryingto make the images to be abled to be clicked

--- js/script.js
+++ js/script.js
@@ -20,6 +20,17 @@ function addanimalstolist(animalarray,classtoadd,newanimals){
 
 $(Document).on('click','searchButton',function(){
     var type = $(this).data('type');
+    GiphyUISDK.configure(apiKey: "s24XBCMj8DNq5COlFa7Kossj8ZzC7U1D");
+    $.ajax({url:queryURL,method:"Get"})
+    .done(function(response){
+        for(var i=0;i<response.data.length;i++){
+            var searchButton = $('<div class="search-item">');
+            var rating = response.data[i].rating;
+            var s = $('<p>').text('Rating', +rating);
+            var animated = response.data[i].images.fixed_height.utl;
+            var still = response.data[i].images.fixed_height_still.url;
+            
 
-    GiphyUISDK.configure(apiKey: "s24XBCMj8DNq5COlFa7Kossj8ZzC7U1D")
+        }
+    })
 })
